1324f76fd2f2bf34ad0a95bc90a4d618eacacee0
Slowship <Joshua.Asprey@gmail.com>
2016-01-28 17:45:15 +0000

    Moved tests to own folder. Updated C5

---
+++ test/AI.Search.UnitTests/SearchFringes/AStarSearchFringeTestFixture.cs
@@ -0,0 +1,53 @@
+﻿using AI.Search.SearchFringes;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AI.Search.UnitTests.SearchFringes
+{
+    public class AStarSearchFringeTestFixture
+    {
+        IFixture fixture;
+        Mock<IHuristic<TestState>> huristicMock;
+
+        public class TestState : ICostTracked
+        {
+            public int Cost { get; }
+
+            public TestState(int cost)
+            {
+                Cost = cost;
+            }
+        }
+
+        public AStarSearchFringeTestFixture()
+        {
+            fixture = new Fixture().Customize(new AutoMoqCustomization());
+            huristicMock = fixture.Freeze<Mock<IHuristic<TestState>>>();
+        }
+
+        [Fact]
+        public void GetQueueOrder_ShouldReturnAStatesHuristicValue()
+        {
+            // Arrange
+            var huristicVal = fixture.Create<int>();
+
+            var state = new TestState(huristicVal);
+            huristicMock.Setup(h => h.GetHuristicValue(state)).Returns(huristicVal);
+
+            var subject = fixture.Create<AStarSearchFringe<TestState>>();
+
+            // Act
+            var result = subject.GetQueueOrder(state);
+
+            // Assert
+            Assert.Equal(state.Cost + huristicVal, result);
+        }
+    }
+}
---
+++ test/AI.Search.UnitTests/SearchFringes/GreedySearchFringeTestFixture.cs
@@ -0,0 +1,46 @@
+﻿using AI.Search.SearchFringes;
+using C5;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AI.Search.UnitTests.SearchFringes
+{
+    public class GreedySearchFringeTestFixture
+    {
+        IFixture fixture;
+        Mock<IHuristic<TestState>> huristicMock; 
+
+        public class TestState { }
+
+        public GreedySearchFringeTestFixture()
+        {
+            fixture = new Fixture().Customize(new AutoMoqCustomization());
+            huristicMock = fixture.Freeze<Mock<IHuristic<TestState>>>();
+        }
+
+        [Fact]
+        public void GetQueueOrder_ShouldReturnAStatesHuristicValue()
+        {
+            // Arrange
+            var expected = fixture.Create<int>();
+
+            var state = new TestState();
+            huristicMock.Setup(h => h.GetHuristicValue(state)).Returns(expected);
+
+            var subject = fixture.Create<GreedySearchFringe<TestState>>();
+
+            // Act
+            var result = subject.GetQueueOrder(state);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
